f98f1d5d85fc83d4d16a51db3fa7521241ce11dc
Louiepogi <sorianolouie50@gmail.com>
2023-11-21 15:13:56 +0800

    solo 11-21-23

---
+++ framesets/script.js
@@ -0,0 +1,29 @@
+function toggleVenusInfo() {
+  var venus = document.querySelector(".venusInfo");
+  venus.classList.toggle("active");
+}
+
+// function animate() {
+//   this.classList.toggle("rotate");
+// }
+
+// document.getElementById("sunButton").addEventListener("click", animate());
+// document.getElementById("mercuryButton").addEventListener("click", animate());
+// document.getElementById("earthButton").addEventListener("click", animate());
+
+// function createStars() {
+//   const container = document.querySelector("body");
+//   for (let i = 0; i < 1000; i++) {
+//     const star = document.createElement("div");
+//     star.className = "star";
+//     star.style.width = ".1px";
+//     star.style.height = ".1px";
+
+//     star.style.top = Math.random() + 100 + "%";
+//     star.style.left = Math.random() + 100 + "%";
+
+//     container.appendChild(star);
+//   }
+// }
+
+// createStars();
