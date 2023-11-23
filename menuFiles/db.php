5515b135aae446a9261200a07ac1b4dded887e84
Louiepogi <sorianolouie50@gmail.com>
2023-11-23 10:35:07 +0800

    asd

--- menuFiles/db.php
+++ menuFiles/db.php
@@ -2,9 +2,16 @@
     $dbServer = "localhost";
     $dbUser = "root";
     $dbPass = "";
-    $dbName = "cc101";
+    $dbName = "dbusers";
 
     $conn = "";
 
     $conn = mysqli_connect($dbServer, $dbUser, $dbPass, $dbName);
 ?> 
+
+<!-- for 000webhost
+    $dbServer = "localhost";
+    $dbUser = "id21556917_hakaii";
+    $dbPass = "Lolomoko@23";
+    $dbName = "id21556917_cc101";
+-->
