86f3c913bf4338a5c0975e38186e4094ae20545a
Louiepogi <sorianolouie50@gmail.com>
2023-11-30 23:40:22 +0800

    Asd

--- menuFiles/feedback.php
+++ menuFiles/feedback.php
@@ -5,10 +5,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../logos/logowhitebg.png">
     <link rel="stylesheet" href="menu.css">
+    <script src="https://kit.fontawesome.com/ae60d6c6cb.js" crossorigin="anonymous"></script>
     
     <title>Feedback</title>
+
+    <style>
+        .icon {
+            color: black;
+        }
+        a {
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body class="feedback" style="overflow-y: hidden;">
+<nav class="sidebar" style="position: fixed;">
+        <ul>
+            <a href="../home.html"><li class="first">
+                <div class="home-icon">
+                <div class="roof">
+                    <div class="roof-edge"></div>
+                </div>
+                <div class="front"></div>
+                </div>
+            </li></a>
+            <a href="../infoWindow.html"><li class="second">
+                <div class="details-icon">
+                <div class="paper"></div>
+                <div class="lines"></div>
+                <div class="lines"></div>
+                <div class="lines"></div>
+                </div>
+            </li></a>
+            <a href="../explore.html"><li class="third">
+                <div class="explore-icon">
+                <i class="fa-regular fa-compass fa-2x icon"></i>
+                </div>
+            </li></a>
+            <a href="aboutUs.html"><li class="fourth">
+                <div class="about-icon">
+                <div class="head">
+                    <div class="eyes"></div>
+                    <div class="beard"></div>
+                </div>
+                </div>
+            </li></a>
+            <a href="credits.html"><li class="fifth">
+                <div class="about-icon">
+                    <i class="fa-solid fa-copyright fa-2x icon"></i>
+                </div>
+            </li></a>
+            <a href="feedback.php"><li class="sixth" style="height: 83px;">
+                <div class="feedback" style="height: 83px;">
+                    <i class="fa-solid fa-comment fa-2x icon"></i>
+                </div>
+            </li></a>
+            </ul>
+        </nav>
+
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="form" method="post">
 
     <p class="title">FEEDBACK</p>
@@ -32,7 +86,7 @@
     <input type="submit" name="submit" value="Submit" class="submit">
     </form>
     
-    <a href="../home.html" target="_top"><button type="button" class="home" id="home2"></button></a>
+    <!-- <a href="../home.html" target="_top"><button type="button" class="home" id="home2"></button></a> -->
 
 </body>
 </html>
